1f1ffed1ad471f19a2b82e40e2c55c717dda47a0
Jerome Doran <disneyfan9184@gmail.com>
2023-09-16 08:32:14 -0400

    initial project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <div className='p-4'>
+      <Button size='sm' variant='outline'>
+        Click Me
+      </Button>
+    </div>
+  );
+}
