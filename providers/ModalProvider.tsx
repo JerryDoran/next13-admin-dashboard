5f72e59c12dd0801b77bccb13c7f60a1acefcb04
Jerome Doran <disneyfan9184@gmail.com>
2023-09-16 13:39:10 -0400

    added global state management for modals

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+// components
+import StoreModal from '@/components/modals/StoreModal';
+
+export default function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
+}
