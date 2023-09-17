f9da5573e4f33bf591e0809e1f43fe75e6a32d68
Jerome Doran <disneyfan9184@gmail.com>
2023-09-17 14:19:08 -0400

    added mysql database and prisma to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 // components
 import ModalProvider from '@/providers/ModalProvider';
+import prismadb from '@/lib/prismadb';
 
 const inter = Inter({ subsets: ['latin'] });
 
