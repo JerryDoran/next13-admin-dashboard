25f1e6ce204eb67fdc73ae37371de7af53e98506
Jerome Doran <disneyfan9184@gmail.com>
2023-10-01 09:40:07 -0400

    added update and delete functionality for stores

--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -5,45 +5,99 @@ import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { Store } from '@prisma/client';
 import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
 
 // components
 import Heading from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
 import { Trash } from 'lucide-react';
 import { Separator } from '@/components/ui/separator';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from './ui/input';
 
 type Props = {
   initialData: Store;
 };
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(2),
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export default function SettingsForm({ initialData }: Props) {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
+
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
   });
 
-  const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
-
   async function onSubmit(values: SettingsFormValues) {
-    console.log(values);
+    try {
+      await axios.patch(`/api/stores/${params.storeId}`, values);
+    } catch (error) {
+      toast.error('Something went wrong!');
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <>
       <div className='flex items-center justify-between'>
         <Heading title='Settings' description='Manage store preferences' />
-        <Button variant='destructive' size='icon' onClick={() => {}}>
+        <Button
+          disabled={loading}
+          variant='destructive'
+          size='icon'
+          onClick={() => setOpen(true)}
+        >
           <Trash className='h-4 w-4' />
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-full'
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder='Store name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto' type='submit'>
+            Save Changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 }
