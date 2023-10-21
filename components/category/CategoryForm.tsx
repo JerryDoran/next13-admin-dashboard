b525d0f95d82d01b050c652db8d0947fa8e3a807
Jerome Doran <disneyfan9184@gmail.com>
2023-10-21 18:36:30 -0400

    added new category page and components

---
+++ components/category/CategoryForm.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { useState } from 'react';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { Category } from '@prisma/client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { Trash } from 'lucide-react';
+
+// components
+import Heading from '@/components/ui/heading';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import AlertModal from '@/components/modals/AlertModal';
+
+const formSchema = z.object({
+  name: z.string().min(2),
+  billboardId: z.string().min(1),
+});
+
+type CategoryFormValues = z.infer<typeof formSchema>;
+
+type Props = {
+  initialData: Category | null;
+};
+
+export default function CategoryForm({ initialData }: Props) {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
+
+  const title = initialData ? 'Edit category' : 'Create category';
+  const description = initialData ? 'Edit a category' : 'Add a new category';
+  const toastMessage = initialData ? 'Category updated.' : 'Category created.';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  const form = useForm<CategoryFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      billboardId: '',
+    },
+  });
+
+  async function onSubmit(values: CategoryFormValues) {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          values
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, values);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/billboards`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error('Something went wrong!');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function onDelete() {
+    try {
+      setLoading(true);
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      );
+      router.refresh();
+      router.push(`/${params.storeId}/billboards`);
+      toast.success('Billboard deleted.');
+    } catch (error) {
+      toast.error(
+        'Make sure to remove all categories using this billboard first!'
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  }
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        loading={loading}
+        onConfirm={onDelete}
+      />
+      <div className='flex items-center justify-between'>
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant='destructive'
+            size='icon'
+            onClick={() => setOpen(true)}
+          >
+            <Trash className='h-4 w-4' />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='w-full space-y-8'
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder='Category name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto' type='submit'>
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
